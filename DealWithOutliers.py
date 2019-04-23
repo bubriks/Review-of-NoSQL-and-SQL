f5da5118d4f885bae7d8692d9805be00320c74df
Ralf <bubriks@gmail.com>
2019-04-23 16:59:17 +0200

    small fix

--- DealWithOutliers.py
+++ DealWithOutliers.py
@@ -6,7 +6,7 @@ df = pd.read_csv('results/'+count+'.csv')
 
 #removes outliers
 for val in df[['1','2','3','4','5','6','7','8','9','10']]:
-    df[val] = np.where(np.abs(df[val]-df.median(axis=1)) >= (2*df.std(axis=1)), np.nan, df[val])
+    df[val] = np.where(np.abs((df[val]-df.median(axis=1))/df.std(axis=1)) > 3, np.nan, df[val])
 
 #replaces nan with mean of row
 df[['1','2','3','4','5','6','7','8','9','10']] = df[['1','2','3','4','5','6','7','8','9','10']].apply(lambda row: row.fillna(row.mean()), axis=1)
